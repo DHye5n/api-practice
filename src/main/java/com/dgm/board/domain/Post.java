7898782e3e88496340c3ae92979a6a39121d3e2b
dhy25n <sai4875@naver.com>
2024-07-20 13:47:56 +0900

    2-1차 - API (D) 작업 완료

--- src/main/java/com/dgm/board/domain/Post.java
+++ src/main/java/com/dgm/board/domain/Post.java
@@ -16,36 +16,4 @@ public class Post {
     private String body;
     private ZonedDateTime createdDateTime;
 
-
-//    public Post(Long postId, String body, ZonedDateTime createdDateTime) {
-//        this.postId = postId;
-//        this.body = body;
-//        this.createdDateTime = createdDateTime;
-//    }
-//
-//    public Long getPostId() {
-//        return postId;
-//    }
-//
-//    public String getBody() {
-//        return body;
-//    }
-//
-//    public ZonedDateTime getCreatedDateTime() {
-//        return createdDateTime;
-//    }
-//
-//    public void setPostId(Long postId) {
-//        this.postId = postId;
-//    }
-//
-//    public void setBody(String body) {
-//        this.body = body;
-//    }
-//
-//    public void setCreatedDateTime(ZonedDateTime createdDateTime) {
-//        this.createdDateTime = createdDateTime;
-//    }
-
-
 }
